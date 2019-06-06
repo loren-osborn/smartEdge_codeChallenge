63ab0649854bd62e5dd2c8fc94db6f5161424569
Loren Osborn <linux_dr@yahoo.com>
2019-06-06 04:01:18 -0500

    Refactored default mocks tests

--- testtools/mocks/deps_test.go
+++ testtools/mocks/deps_test.go
@@ -9,44 +9,41 @@ import (
 	"testing"
 )
 
-// TestNewDefaultMockDeps tests supplier of default mocks for CodeChallenge tests.
-func TestNewDefaultMockDeps(t *testing.T) {
-	for _, tc := range []struct {
-		fakeInContent  string
-		fakeOutContent string
-		fakeErrContent string
-		fakeArgList    []string
-		homeDirPath    string
-		fakeFileSystem *map[string]*string
-		fakeExitStatus int
-	}{
-		{
-			fakeInContent:  "sample input",
-			fakeOutContent: "sample output",
-			fakeErrContent: "sample error",
-			fakeArgList:    []string{"progName"},
-			homeDirPath:    "/home/me",
-			fakeFileSystem: &map[string]*string{},
-			fakeExitStatus: 7,
-		},
-		{
-			fakeInContent:  "other\n\ttest\tinput  \n",
-			fakeOutContent: "I have a result!",
-			fakeErrContent: "but I stubbed my toe",
-			fakeArgList:    []string{"codechallenge", "-rsa"},
-			homeDirPath:    "/root/",
-			fakeFileSystem: &map[string]*string{},
-			fakeExitStatus: 3,
-		},
-	} {
-		// The arguments to mocks.NewDefaultMockDeps() will be:
-		//     fakeInContent:  The content of the fake os.Stdin
-		//     fakeArgList:    The args list the program is being invoked with
-		//     homeDirPath:    Path of the user's home dir
-		//     fakeFileSystem: The content of the fake filesystem where keys
-		//         are to be stored.
-		// We explicitly don't care what the name of the return type is, only
-		// it's behavior:
+type mockEnvTestCaseInfo struct {
+	fakeInContent  string
+	fakeOutContent string
+	fakeErrContent string
+	fakeArgList    []string
+	homeDirPath    string
+	fakeFileSystem *map[string]*string
+	fakeExitStatus int
+}
+
+var mockEnvTestCases []mockEnvTestCaseInfo = []mockEnvTestCaseInfo{
+	{
+		fakeInContent:  "sample input",
+		fakeOutContent: "sample output",
+		fakeErrContent: "sample error",
+		fakeArgList:    []string{"progName"},
+		homeDirPath:    "/home/me",
+		fakeFileSystem: &map[string]*string{},
+		fakeExitStatus: 7,
+	},
+	{
+		fakeInContent:  "other\n\ttest\tinput  \n",
+		fakeOutContent: "I have a result!",
+		fakeErrContent: "but I stubbed my toe",
+		fakeArgList:    []string{"codechallenge", "-rsa"},
+		homeDirPath:    "/root/",
+		fakeFileSystem: &map[string]*string{},
+		fakeExitStatus: 3,
+	},
+}
+
+// TestNewDefaultMockDepsNotNil tests supplier of default mocks for
+// CodeChallenge tests is populated.
+func TestNewDefaultMockDepsNotNil(t *testing.T) {
+	for _, tc := range mockEnvTestCases {
 		mockDepsBundle := mocks.NewDefaultMockDeps(
 			tc.fakeInContent, tc.fakeArgList, tc.homeDirPath, tc.fakeFileSystem)
 		if mockDepsBundle == nil {
@@ -56,15 +53,27 @@ func TestNewDefaultMockDeps(t *testing.T) {
 		if deps == nil {
 			t.Error("mockDepsBundle.Deps should never be nil.")
 		}
-		// Verify Stdin properly populated
+	}
+}
+
+// TestNewDefaultMockDepsStdin tests mocks read from Stdin.
+func TestNewDefaultMockDepsStdin(t *testing.T) {
+	for _, tc := range mockEnvTestCases {
+		mockDepsBundle := mocks.NewDefaultMockDeps(
+			tc.fakeInContent, tc.fakeArgList, tc.homeDirPath, tc.fakeFileSystem)
 		if actualInput, err := ioutil.ReadAll(mockDepsBundle.Deps.Os.Stdin); err != nil {
 			t.Error(err.Error())
 		} else if string(actualInput) != tc.fakeInContent {
 			t.Errorf("mockDepsBundle.Deps.Os.Stdin was expected to supply\n\t%#v but instead yeilded\n\t%#v.", tc.fakeInContent, string(actualInput))
 		}
+	}
+}
 
-		// Verify connection between Stdout and Stderr writers and
-		// corresponding buffers.
+// TestNewDefaultMockDepsStdoutAndStderr tests mocks write to fake Stdout and Stderr.
+func TestNewDefaultMockDepsStdoutAndStderr(t *testing.T) {
+	for _, tc := range mockEnvTestCases {
+		mockDepsBundle := mocks.NewDefaultMockDeps(
+			tc.fakeInContent, tc.fakeArgList, tc.homeDirPath, tc.fakeFileSystem)
 		for _, writerInfo := range []struct {
 			content string
 			name    string
@@ -95,6 +104,15 @@ func TestNewDefaultMockDeps(t *testing.T) {
 				}
 			}
 		}
+	}
+}
+
+// TestNewDefaultMockDepsExitStatus tests faked exit mechanism is operating.
+func TestNewDefaultMockDepsExitStatus(t *testing.T) {
+	for _, tc := range mockEnvTestCases {
+		mockDepsBundle := mocks.NewDefaultMockDeps(
+			tc.fakeInContent, tc.fakeArgList, tc.homeDirPath, tc.fakeFileSystem)
+
 		if mockDepsBundle.GetExitStatus() != 0 {
 			t.Errorf("mockDepsBundle.GetExitStatus() should default to 0. Got %#v instead.", mockDepsBundle.GetExitStatus())
 		}
@@ -109,41 +127,5 @@ func TestNewDefaultMockDeps(t *testing.T) {
 		if codeAfterExitExecuted {
 			t.Error("Call to mockDepsBundle.Deps.Os.Exit() didn't interrupt flow of execution.")
 		}
-
 	}
-
-	// osExitHarness := mocks.NewOsExitMockHarness()
-	// stdin := bytes.NewBufferString("sample input")
-	// stdout := &bytes.Buffer{}
-	// stderr := &bytes.Buffer{}
-	// codechallenge.RealMain(&codechallenge.Dependencies{
-	// 	Os: codechallenge.OsDependencies{
-	// 		Stdin:  stdin,
-	// 		Stdout: stdout,
-	// 		Stderr: stderr,
-	// 		Exit:   osExitHarness.GetMock(),
-	// 	},
-	// })
-	// if exitStatus := osExitHarness.GetExitStatus(); exitStatus != 0 {
-	// 	t.Errorf("RealMain() should have a normal exit status of 0. Got %#v instead.", exitStatus)
-	// }
 }
-
-// // TestSerializeJSON tests the population and serialization of the response.
-// func TestSerializeJSON(t *testing.T) {
-// 	osExitHarness := mocks.NewOsExitMockHarness()
-// 	stdin := bytes.NewBufferString("sample input")
-// 	stdout := &bytes.Buffer{}
-// 	stderr := &bytes.Buffer{}
-// 	codechallenge.RealMain(&codechallenge.Dependencies{
-// 		Os: codechallenge.OsDependencies{
-// 			Stdin:  stdin,
-// 			Stdout: stdout,
-// 			Stderr: stderr,
-// 			Exit:   osExitHarness.GetMock(),
-// 		},
-// 	})
-// 	if exitStatus := osExitHarness.GetExitStatus(); exitStatus != 0 {
-// 		t.Errorf("RealMain() should have a normal exit status of 0. Got %#v instead.", exitStatus)
-// 	}
-// }
